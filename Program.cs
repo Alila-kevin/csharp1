e34924171a6d48e344ef4ef86ca85ee4e11fff80
unknown <Administrator@ICTINTERN-PC.Billserver.kiwasco.co.ke>
2023-05-11 14:23:37 +0300

    we start

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("What is your name buddy's");
+        string? username = Console.ReadLine();
+        Console.WriteLine("You have a nice name "+username+" "+ "welcome to c sharp program");
+       
+        Console.ReadKey();
+    }
+}

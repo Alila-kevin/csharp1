72be0e86828284ab65f8befa432d7e495d35e6d4
Alila-kevin <85939629+Alila-kevin@users.noreply.github.com>
2023-05-12 12:31:45 +0300

    Update Program.cs
    update

--- Program.cs
+++ Program.cs
@@ -3,10 +3,21 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("What is your name buddy's");
+        int num1;
+        int num2;
+        double result;
+
+        Console.WriteLine("What is your name ?");
         string? username = Console.ReadLine();
         Console.WriteLine("You have a nice name "+username+" "+ "welcome to c sharp program");
-       
+        Console.WriteLine("write the first number");
+         num1=Convert.ToInt32(Console.ReadLine());
+         Console.WriteLine("your first number is: "+num1);
+        Console.WriteLine("write the second number"); 
+         num2=Convert.ToInt32(Console.ReadLine());
+         Console.WriteLine("Your second number is: "+num2);
+         result=num1*num2;
+       Console.WriteLine(username+" Your answer is: "+result);
         Console.ReadKey();
     }
 }
